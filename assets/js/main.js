48f73561d76a5921da908ab8660f4377936d70ea
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-03-10 01:35:24 +0100

    new file:   assets/css/styles.css 	new file:   assets/img/damefrokost.png 	new file:   assets/img/nav_logo.png 	new file:   assets/js/main.js 	new file:   assets/json/events.json 	modified:   index.html

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*-------------------- EVENTS --------------------*/
+let events = null;
+fetch('/assets/json/events.json')
+.then(response => response.json())
+.then(data => {
+    events = data;
+    console.log(events);
+    addDataToHTML();
+})
+
+let listEvents = document.querySelector('.listEvents');
+function addDataToHTML(){
+    events.forEach(event => {
+        // Create new Element item
+        let newEvent = document.createElement('a');
+        newEvent.href = '/event.html?id=' + event.id;
+        newEvent.classList.add('event-item');
+        newEvent.innerHTML =  `
+            <img src="${event.image}">
+            <h2>${event.title}</h2>
+            <h3>${event.location}</h3>
+            <span>${event.description}</span>
+
+        `
+
+        // Add this element in listEvents class
+        listEvents.appendChild(newEvent);
+    });
+}
